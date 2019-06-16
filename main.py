1238a0aacac1102b6871beb1171f9787a692c1e4
Henry Rossiter <henry.rossiter@utexas.edu>
2019-06-16 08:07:30 -0500

    pretty printing, remove old script

--- main.py
+++ main.py
@@ -8,50 +8,49 @@ repository_name = config.repository[config.repository.rfind('/')+1:]
 build_branch = config.build_branch
 wait_time = config.wait_time
 
-print('\n------------------------------------------')
-print('--- Launching CD Scripts for {} ---'.format(repository_name))
-print('------------------------------------------\n')
 
+def bordered_print(msg):
+    length = len(msg)
+    print('\n')
+    print('-' * length)
+    print(msg)
+    print('-' * length)
+
+
+bordered_print('Launching CD Scripts for {}'.format(repository_name))
 
 if not os.path.exists(repository_name):
-    print('Cloning {}'.format(repository_name))
+    bordered_print('Cloning {}'.format(repository_name))
     subprocess.run(['git', 'clone', config.repository])
 
-print('\n-------------------------------')
-print('--- Installing dependencies ---')
-print('-------------------------------\n')
+bordered_print('Installing dependencies')
 installer = subprocess.Popen(
                              'npm install',
                              shell=True,
                              cwd=repository_name)
 installer.wait()
 
-print('\n-------------------------------')
-print('--- Launching Server ---')
-print('-------------------------------\n')
+bordered_print('Launching Server')
 subprocess.Popen(
                  'npm start',
                  shell=True,
                  cwd=repository_name)
 
 while True:
-    print('\n--------------------------------------')
-    print('Pulling changes from {} branch'.format(build_branch))
-    print('--------------------------------------\n')
-    subprocess.Popen(
+    bordered_print('Pulling changes from {} branch'.format(build_branch))
+    fetcher = subprocess.Popen(
                      'git fetch origin',
                      shell=True,
                      cwd=repository_name)
-    subprocess.Popen(
+    fetcher.wait()
+
+    resetter = subprocess.Popen(
                      'git reset --hard origin/{}'.format(build_branch),
                      shell=True,
                      cwd=repository_name)
+    resetter.wait()
 
-    time.sleep(5)
-
-    print('\n-----------------------------')
-    print('--- Updating dependencies ---')
-    print('-------------------------------\n')
+    bordered_print('Updating dependencies')
     installer = subprocess.Popen(
                      'npm install',
                      shell=True,
